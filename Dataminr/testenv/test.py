f31c26c85497391e9b6d7c9996596673fdc10744
mohan-aimleap <mohan.b@aimleap.com>
2025-05-27 16:50:34 +0530

    tesT

--- Dataminr/testenv/test.py
+++ Dataminr/testenv/test.py
@@ -1,3 +1,4 @@
 print("Hello test!!!")
 print("This is a test file in the testenv directory.")
 print("This file is part of the Dataminr project.")
+print("This file is used to test the environment setup.")
